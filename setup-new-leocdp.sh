d463d1f5f5df1222afc7fc95d335199fda53ea8d
Trieu Nguyen <tantrieuf31.database@gmail.com>
2021-08-28 15:22:02 +0700

    add setup-new-leocdp.sh

--- setup-new-leocdp.sh
+++ setup-new-leocdp.sh
@@ -4,6 +4,7 @@ BUILD_VERSION="v_0.20210815-23h"
 JAR_MAIN="leo-main-starter-$BUILD_VERSION.jar"
 
 echo "Enter the superadmin-password: "  
-read superadmin_password  
+read superadmin_password 
+echo "Username: superadmin and password: $superadmin_password" 
 
-java -jar $JAR_MAIN init-leocdp-with-superadmin-password $superadmin_password
+java -jar $JAR_MAIN setup-leocdp-with-password $superadmin_password
